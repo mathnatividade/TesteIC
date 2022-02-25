bb4e9ce72669d5e82e6a2caf3a77767cb45e7405
mathnatividade <mathnatividade@gmail.com>
2022-02-25 04:08:40 -0300

    Commit inicial

---
+++ appfastpapi/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+
+app = FastAPI()
+
+@app.get("/status", status_code=200)
+def healthcheck():
+    return 'http status code 200'
+
+@app.get("/")
+def homescore():
+    return 'ok'
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT')))
