a33c748adf825399efc6ec40d361b3e53ddf80f4
mathnatividade <mathnatividade@gmail.com>
2022-02-25 19:54:25 -0300

    Remoção acesso raiz app e diretório renomeado

--- appfastapi/app/main.py
+++ appfastapi/app/main.py
@@ -8,9 +8,9 @@ app = FastAPI()
 def healthcheck():
     return 'http status code 200'
 
-@app.get("/")
-def homescore():
-    return 'ok'
+# @app.get("/")
+# def homescore():
+#    return 'ok'
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT')))
