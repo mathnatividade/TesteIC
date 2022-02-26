40b1ce9f0f732e32e916bc62c52bdf251166c4d2
mathnatividade <mathnatividade@gmail.com>
2022-02-26 17:21:21 -0300

    Acerto faixa ip no firewall

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -8,7 +8,7 @@ iptables -A INPUT -p tcp --destination-port 22 -j ACCEPT
 iptables -A INPUT -p tcp --destination-port 80 -j ACCEPT
 
 #Libera acesso as portas de 9000 a 9010 apenas a rede local
-iptables -A INPUT -p tcp --syn -s 172.10.0.0/24 --dport 9000:9010 -j ACCEPT
+iptables -A INPUT -p tcp --syn -s 192.168.1.0/24 --dport 9000:9010 -j ACCEPT
 #iptables -A INPUT -p tcp --destination-port 9000:9010 -s 172.10.0.0/255.255.255.0 -j ACCEPT
 
 #Fechando portas 
