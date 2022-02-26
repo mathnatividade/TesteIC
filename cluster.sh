c7d12d62d9b667eb687ed2d68dd08fc7de4930ca
mathnatividade <mathnatividade@gmail.com>
2022-02-26 19:36:18 -0300

    Acerto argumentos

--- cluster.sh
+++ cluster.sh
@@ -3,19 +3,19 @@
 #Gestão stack de aplicações docker
 
 case $1 in
-   "-start") echo "Iniciando stack de aplicações no docker."
+   "start") echo "Iniciando stack de aplicações no docker."
              sudo docker-compose up -d
              #Inicia stack de aplicações de acordo com o docker-compose.yml
          ;;
-   "-stop") echo "Parando stack de aplicações no docker"
+   "stop") echo "Parando stack de aplicações no docker"
             sudo docker-compose stop
             #Finaliza stack de aplicações de acordo com o docker-compose.yml
          ;;
-   "-status") echo "Status da stack de aplicações no docker."
+   "status") echo "Status da stack de aplicações no docker."
               sudo docker ps -a
               #Exibe o status da stack de aplicações no docker.
          ;;
-   "-del") echo "Parando e removendo stack de aplicações no docker"
+   "del") echo "Parando e removendo stack de aplicações no docker"
            sudo docker-compose down
            #Comando extra, para e remove os containers no docker.
          ;;
