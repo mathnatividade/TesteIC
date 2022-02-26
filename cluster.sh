35135e78c9269bad763c6fa0ec70a94dd7a3c4bf
mathnatividade <mathnatividade@gmail.com>
2022-02-26 17:59:39 -0300

    Adição script gestão da stack

---
+++ cluster.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Gestão stack de aplicações docker
+
+case $1 in
+   "-start") echo "Iniciando stack de aplicações no docker."
+             sudo docker-compose up -d
+             #Inicia stack de aplicações de acordo com o docker-compose.yml
+         ;;
+   "-stop") echo "Parando stack de aplicações no docker"
+            sudo docker-compose stop
+            #Finaliza stack de aplicações de acordo com o docker-compose.yml
+         ;;
+   "-status") echo "Status da stack de aplicações no docker."
+              sudo docker ps -a
+              #Exibe o status da stack de aplicações no docker.
+         ;;
+   "-del") echo "Parando e removendo stack de aplicações no docker"
+           sudo docker-compose down
+           #Comando extra, para e remove os containers no docker.
+         ;;
+   *) echo "Opção inválida!"
+      exit 1
+      ;;
+esac
