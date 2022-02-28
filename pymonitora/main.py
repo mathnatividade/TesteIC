419282155b2f691c3ca46fcdf04df5a8189aee4e
mathnatividade <mathnatividade@gmail.com>
2022-02-28 16:31:05 -0300

    Teste scrapy

--- pymonitora/main.py
+++ pymonitora/main.py
@@ -1,6 +1,19 @@
 import requests
 import urllib, urllib.request
+#import scrapy
 
+#class QuotesSpider(scrapy.Spider):
+#    name = 'QuotesSpider'
+#    start_urls = ['http://quotes.toscrape.com/']
+#
+#    def parse(self, response):
+#        quotes = response.xpath('*//div[@class="quote]')
+#        for q in quotes:
+#            yield {
+#                'title':q.xpath('.//span[@class="text"]/text()').get(),
+#                'author':q.xpath('.//small[@class="author"]/text()').get(),
+#                'tags':q.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').getall()
+#            }
 
 #Testa acesso ao container nginx
 try :
@@ -17,8 +30,10 @@ try :
 except urllib.request.URLError:
     print ('App1 is down\n')
 else  :
-    responseapp1 = requests.get('http://192.168.1.72/app1/status')
+    responseapp1 = requests.get('http://192.168.1.72/app1/status') 
     print ("App1 is up.","Http code:", responseapp1.status_code,'\n')
+#    elif responseapp1.status_code==502 :
+#        print ("App1 is down, but nginx server is up.","Http code:", responseapp1.status_code,'\n')
 
 #Testa acesso ao container app2
 try :
