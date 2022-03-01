485748b34d86b13313bcf28dc2ae403c6eae6cbc
mathnatividade <mathnatividade@gmail.com>
2022-03-01 20:42:44 -0300

    teste

--- appfastapi/app/main.py
+++ appfastapi/app/main.py
@@ -1,22 +1,17 @@
-from fastapi import FastAPI
-from datetime import datetime
+from fastapi import FastAPI, Response, status
 import uvicorn
 import socket
 import os
-import pytz
 
 app = FastAPI()
 
 @app.get("/status", status_code=200)
-def status(): 
+def status(response: Response):
     hostname = socket.gethostname()
     ip = socket.gethostbyname(socket.gethostname())
-    IST = pytz.timezone('America/Sao_Paulo') 
-    datetime_ist = datetime.now(IST) 
-    acesstime = datetime_ist.strftime('%A, %B %d, %Y %H:%M:%S')
+    #response.status_code = status.HTTP
+    print(status)
     return {"name": hostname, "ip": ip}
-
-    #"code": 200, "time":acesstime
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT')))
