a375dc92e95874bd1ae07f8bf78850be029c5950
mathnatividade <mathnatividade@gmail.com>
2022-03-02 11:53:11 -0300

    Liberação de portas na rede local

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -9,7 +9,9 @@ iptables -A INPUT -p tcp --destination-port 80 -j ACCEPT
 iptables -A INPUT -p tcp --destination-port 443 -j ACCEPT
 
 #Libera acesso as portas de 9000 a 9010 apenas a rede local
-iptables -A INPUT -p tcp --syn -s 172.10.0.0/24 --dport 9000:9010 -j ACCEPT
+iptables -A INPUT -p tcp -s 192.168.1.0/24 -d 192.168.1.72 --dport 9000:9010 -j ACCEPT
+iptables -A OUTPUT -p tcp -s 192.168.1.72 -d 192.168.1.0/24 --dport 9000:9010 -j ACCEPT
+#iptables -A INPUT -p tcp --syn -s 172.10.0.0/24 --dport 9000:9010 -j ACCEPT
 #iptables -A INPUT -p tcp --syn -s 192.168.1.0/24 --dport 9000:9010 -j ACCEPT
 #iptables -A INPUT -p tcp --destination-port 9000:9010 -s 172.10.0.0/255.255.255.0 -j ACCEPT
 
