782aa84c49ba7f57f5b4392bce5ffb223bf5505f
Matheus Natividade <41765994+mathnatividade@users.noreply.github.com>
2023-07-03 20:23:08 -0300

    Update firewall.sh

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -11,13 +11,6 @@ iptables -A INPUT -p tcp --destination-port 443 -j ACCEPT
 #Libera acesso as portas de 9000 a 9010 apenas a rede local
 iptables -A INPUT -p tcp -s 192.168.1.0/24 -d 192.168.1.72 --dport 9000:9010 -j ACCEPT
 iptables -A OUTPUT -p tcp -s 192.168.1.72 -d 192.168.1.0/24 --dport 9000:9010 -j ACCEPT
-#iptables -A INPUT -p tcp --syn -s 172.10.0.0/24 --dport 9000:9010 -j ACCEPT
-#iptables -A INPUT -p tcp --syn -s 192.168.1.0/24 --dport 9000:9010 -j ACCEPT
-#iptables -A INPUT -p tcp --destination-port 9000:9010 -s 172.10.0.0/255.255.255.0 -j ACCEPT
-
 
 #Fechando as outras portas 
 iptables -A INPUT -p tcp --syn -j DROP
-
-#Rejeita ping
-#echo "1" > /proc/sys/net/ipv4/icmp_echo_ignore_all
